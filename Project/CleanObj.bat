69fc8ef8ab22be40821fb149187ac210804dfe68
opentechshare <opentechshare@sina.com>
2015-01-31 15:40:32 +0800

    edit test

--- Project/CleanObj.bat
+++ Project/CleanObj.bat
@@ -1,7 +1,7 @@
 del /Q %~dp0\Obj\*.*
 del /Q %~dp0\List\*.*
 del /Q %~dp0\JLink*
-del /Q %~dp0\project.uvgui.*
+del /Q %~dp0\test.uvgui.*
 del /Q %~dp0\*.bak
 del /Q %~dp0)\*.dep
 rd /Q /S %~dp0\Obj
